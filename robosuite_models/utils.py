14beed9b343e128e10c27c56f9f543ea05f3a9ec
Yifeng Zhu <yifeng.zhu@utexas.edu>
2024-10-28 22:07:29 +0800

    Remove outdated variable name

--- robosuite_models/utils.py
+++ robosuite_models/utils.py
@@ -1,7 +1,7 @@
 import os
 
-MENAGERIE_ASSETS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets")
+MODEL_ASSETS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets")
 
 
 def robosuite_model_path_completion(path):
-    return os.path.join(MENAGERIE_ASSETS_PATH, path)
+    return os.path.join(MODEL_ASSETS_PATH, path)
